f0b142738d825f916d2d37ba7c0c11730792755a
Anna <missjellx@gmail.com>
2021-10-19 00:28:54 +0300

    feat: add queue solution

--- src/st-queue.js
+++ src/st-queue.js
@@ -13,18 +13,31 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-  que;
+  constructor() {
+    this.que = null;
+  }
   getUnderlyingList() {
     return this.que;
   }
 
   enqueue(element) {
-    this.que.next = new ListNode(element);
+    if (!this.que) {
+      this.que = new ListNode(element);
+    } else {
+      let node = this.que;
+
+      while (node.next) {
+        node = node.next;
+      }
+      
+      node.next = new ListNode(element);
+    }
   }
 
   dequeue() {
     let value = this.que.value;
     this.que = this.que.next;
+
     return value;
   }
 };
